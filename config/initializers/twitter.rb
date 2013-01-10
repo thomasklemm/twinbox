c02c15b8e66ee00267d36383825f45f59010a994
Thomas Klemm <github@tklemm.eu>
2013-01-10 16:56:28 +0100

    Add twitter gem and config

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,7 @@
+# Twitter consumer setup
+Twitter.configure do |config|
+  config.consumer_key    = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+  config.oauth_token        = ENV['TWITTER_OAUTH_TOKEN']
+  config.oauth_token_secret = ENV['TWITTER_OAUTH_TOKEN_SECRET']
+end
