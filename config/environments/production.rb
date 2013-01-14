e085f19d97f283700e52f2312e7a0d1ca9ab3990
Thomas Klemm <github@tklemm.eu>
2013-01-14 12:57:38 +0100

    Create and update Cloudfront distribution for assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Twinbox::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = 'http://d2ishtm40wfhei.cloudfront.net'
+  config.action_controller.asset_host = 'http://d3ppyuzz0fh1kh.cloudfront.net'
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w()
