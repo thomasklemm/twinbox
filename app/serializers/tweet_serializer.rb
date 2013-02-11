bf76e9f81a33b169a568213983d24f05e100c7b1
Thomas Klemm <github@tklemm.eu>
2013-02-11 14:24:07 +0100

    Remove files

--- app/serializers/tweet_serializer.rb
+++
@@ -1,6 +0,0 @@
-class TweetSerializer < ActiveModel::Serializer
-  attributes :id,
-    :text,
-    :user_name,
-    :user_screen_name
-end
--- db/migrate/20130110155716_create_companies.rb
+++
@@ -1,9 +0,0 @@
-class CreateCompanies < ActiveRecord::Migration
-  def change
-    create_table :companies do |t|
-      t.text :name, null: false
-
-      t.timestamps
-    end
-  end
-end
